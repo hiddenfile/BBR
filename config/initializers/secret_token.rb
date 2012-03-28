afe593d23dbf9f517283dffbdd3771c9be1dc425
Max Dolgih <max.dolgih@faceit.com.ua>
2012-03-29 01:48:38 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BBR::Application.config.secret_token = 'd91ad5257073fb9aa794c66650be14faaa5af6fd733e68458b4ae8bef57a902f81be4178b3c89f2e589cc5e2bb04a76c5f8ad26c3a69da44ef86a15fd700300a'
